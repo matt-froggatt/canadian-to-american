28bb61797645cf553f5db17f2f946f40778c16dc
Matthew Froggatt <mattfr@live.ca>
2019-04-23 00:21:50 -0400

    Added some comments for readability, and shuffled around some functionality just because I feel it makes more sense.

--- catoam.py
+++ catoam.py
@@ -2,12 +2,25 @@ import click
 import translator
 import os
 
+# Denotes the upcoming method as a command, and it is what gets called when the user runs the command (catoam)
 @click.command()
-@click.option("-o", "--output-file", "output_file", default=None, help="Set the output file.")
+# Sets the option -o or --output-file to take in text to parameter output_location and gives a help message
+@click.option("-o", "--output-file", "output_location", default=None, help="Set the output file.")
+# Sets an argument for the command, and outputs it to the parameter filename
 @click.argument("filename")
-def translate(filename, output_file):
-    click.echo("Working: %s" % os.path.abspath(filename))
-    file_translator = translator.Translator(filename)
+# Where the magic happens. Called when the command is run, and uses the parameters from the arguments and options to translate.
+def translate(filename, output_location):
+    click.echo("Translating %s" % os.path.abspath(filename))
+
+    # Sets output file location based on whether output_location is None
+    output_file = output_location if output_location is not None else filename
+    click.echo("Translating to %s" % os.path.abspath(output_file))
+
+    # Creates the the translator, gives it the required info (current filename, and output location), and translates the file
+    file_translator = translator.Translator(filename, output_location)
     file_translator.translate()
-    file_translator.write_to_file(output_file)
-    print("Translation complete.")
+
+    # Writes the translation to the output file
+    file_translator.write_to_file()
+
+    click.echo("Translation complete.")
--- translator.py
+++ translator.py
@@ -1,10 +1,18 @@
 import json
 import re
 
+# Simple class that I use to translate documents easily. Probably doesn't need to be an object, but its more fun this way.
 class Translator:
 
-    def __init__(self, filename):
+
+    # The constructor, just initializes variables, loads the file's text into a string (maybe not the best idea, I'll fix it if needed, but no file should exceed
+    #   the size of memory in most modern computers), and loads the translations JSON file which I use to store all the different translations
+
+    # Note: since the translations are stored as a JSON, they are loaded into Python as a dictionary, and while that is useful in this circumstance, it is only useful
+    #   assuming that every word has a one to one translation.
+    def __init__(self, filename, output):
         self.filename = filename
+        self.output = output
 
         with open(self.filename, "r") as file:
             self.filedata = file.read()
@@ -12,19 +20,29 @@ class Translator:
         with open('translations.json') as f:
             self.translations = json.load(f)
     
+
+    # Just prints out the contents of the input file. It isn't actually part of the CLI, its just a useful debugging tool
     def display(self):
         print(self.filedata)
 
+
+    # Translates the contents of the original file in place using Python's regex system. DOES NOT MODIFY THE ACTUAL FILE, OR PRODUCE OUTPUT. This just runs a simple for loop
+    #   through every possible translatable value, and replaces them with their translations. There might be a faster way to do it, but I feel like in this case mental 
+    #   efficieny is more important than time efficiency.
     def translate(self):
         for key in self.translations:
             regex = re.compile(re.escape(key), re.IGNORECASE)
             self.filedata = regex.sub(self.translations[key], self.filedata)
 
+
+    # Shows all of the implemented translations. Again, it isn't par of the CLI but it is a useful debugging tool.
     def display_translations(self):
         for key in self.translations:
             print(key, self.translations[key])
 
-    def write_to_file(self, output_location):
-        output_file = output_location if output_location is not None else self.filename
-        with open(output_file, "w") as file:
+
+    # Takes the freshly translated file content and writes it out to a file. It takes in an output location, but only uses it if its not set to None. I use None to indicate
+    #   that the user hasn't set their output location (using -o) and so it will just overwrite the initial file with the translation.
+    def write_to_file(self):
+        with open(self.output, "w") as file:
             file.write(self.filedata)
