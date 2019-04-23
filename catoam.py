72fb0a9424a088ed3777f3d0d020dd246fbc1d57
Matthew Froggatt <mattfr@live.ca>
2019-04-22 21:09:03 -0400

    Added some simple test code to catoam and translator.

--- catoam.py
+++ catoam.py
@@ -6,5 +6,5 @@ import translator
 def translate(filename):
     file = open(filename, "r")
     click.echo("Working: %s" %filename)
-    for word in file:
-        click.echo(translator.translate(word))
+    thing = translator.Translator(file)
+    thing.display()
--- translator.py
+++ translator.py
@@ -1,2 +1,9 @@
-def translate(word):
-    return "translated"
+
+class Translator:
+
+    def __init__(self, file):
+        self.file = file
+    
+    def display(self):
+        for line in self.file:
+            print(line)
